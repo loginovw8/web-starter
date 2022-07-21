f8ee2937a5ef83f171eba369de1b755795ffea5c
loginovw8 <loginovw8@gmail.com>
2022-07-21 13:38:36 +0300

    Lesson 11 edited.

--- l11-architecture/backend/app.js
+++ l11-architecture/backend/app.js
@@ -1,27 +1,46 @@
-const express = require('express')
-const mysql = require('mysql')
-const app = express()
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
 
 // Соединение с базой данных
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  database: 'nature',
-  user: 'root',
-  password: 'secret',
-})
+  host: "127.0.0.1",
+  database: "nature",
+  user: "root",
+  password: "secret",
+});
 
-connection.connect(function (err) { if (err) throw err })
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+// Обработка POST-запросов из форм
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Запуск веб-сервера по адресу http://localhost:8080
-app.listen(8080)
+app.listen(8080);
 
 /**
  * Маршруты
  */
-app.get('/', (req, res) => {
-  connection.query('SELECT * FROM items', (err, data, fields) => {
-    if (err) throw err
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM items", (err, data, fields) => {
+    if (err) throw err;
 
-    res.status(200).send(data)
-  })
-})
+    res.status(200).send(data);
+  });
+});
+
+app.post("/store", (req, res) => {
+  console.log(req.body.title);
+  connection.query(
+    "INSERT INTO items (title, image) VALUES (?, ?)",
+    [[req.body.title], [req.body.image]],
+    (err, result) => {
+      if (err) throw err;
+
+      res.status(201).send();
+    }
+  );
+});
--- l11-architecture/frontend/app.js
+++ l11-architecture/frontend/app.js
@@ -1,34 +1,62 @@
-const express = require('express')
-const path = require('path')
-const request = require('request')
-const app = express()
+const express = require("express");
+const path = require("path");
+const request = require("request");
+const app = express();
 
 // Путь к директории файлов ресурсов (css, js, images)
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Настройка шаблонизатора
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Путь к директории файлов отображения контента
-app.set('views', path.join(__dirname, 'views'))
+app.set("views", path.join(__dirname, "views"));
+
+// Обработка POST-запросов из форм
+app.use(express.urlencoded({ extended: true }));
 
 // Запуск веб-сервера по адресу http://localhost:3000
-app.listen(3000)
+app.listen(3000);
 
 /**
  * Маршруты
  */
-app.get('/', (req, res) => {
-  let url = 'http://127.0.0.1:8080'
+app.get("/", (req, res) => {
+  let url = "http://127.0.0.1:8080";
 
   request.get(url, (err, response, body) => {
     if (!err && res.statusCode === 200) {
-
-      res.render('home', {
+      res.render("home", {
         items: JSON.parse(body),
-      })
+      });
     } else {
-      console.log(err)
+      console.log(err);
     }
-  })
-})
+  });
+});
+
+app.post("/store", (req, res) => {
+  let url = "http://127.0.0.1:8080/store";
+
+  request.post(
+    {
+      url: url,
+      json: true,
+      body: {
+        title: req.body.title,
+        image: req.body.image,
+      },
+    },
+    (err, response, body) => {
+      if (!err && response.statusCode == 201) {
+        res.redirect("/");
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
