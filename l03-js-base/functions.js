b6ddff24c1a141885afcdae916b96466ccaada88
loginovw8 <loginovw8@gmail.com>
2023-04-20 11:07:45 +0300

    Lesson 3 edited.

--- l03-js-base/functions.js
+++ l03-js-base/functions.js
@@ -1,42 +1,42 @@
 // Написать функцию для сложения двух чисел
 function add(num1, num2) {
-  // Вывести: "num1 + num2 = result"
+    // Вывести: "num1 + num2 = result"
 }
 
 // Написать функцию для вычитания двух чисел
 function subtract(num1, num2) {
-  // Вывести: "num1 - num2 = result"
+    // Вывести: "num1 - num2 = result"
 }
 
 // Написать функцию для умножения двух чисел
 function multiply(num1, num2) {
-  // Вывести: "num1 * num2 = result"
+    // Вывести: "num1 * num2 = result"
 }
 
 // Написать функцию для деления двух чисел
 function divide(num1, num2) {
-  // Вывести: "num1 / num2 = result"
+    // Вывести: "num1 / num2 = result"
 }
 
 // Написать функцию нахождения процента от числа
 function percentOf(num, percent) {
-  // Вывести: "percent от num = result"
+    // Вывести: "percent от num = result"
 }
 
 // Написать функцию для проверки вхождения числа в диапазон
 function isInRange(num, startRange, endRange) {
-  // Вывести: "число num находится в диапазоне (startRange, endRange)" или
-  // "число num находится вне диапазона (startRange, endRange)"
+    // Вывести: "число num находится в диапазоне (startRange, endRange)" или
+    // "число num находится вне диапазона (startRange, endRange)"
 }
 
 // Написать функцию для проверки четности или нечетности
 function isEvenOrOdd(num) {
-  // Вывести: "num - четное" или "num - нечетное"
+    // Вывести: "num - четное" или "num - нечетное"
 }
 
 function isPrime(num) {
-  // Вывести: true если "num простое число" или
-  // false если "num непростое число"
+    // Вывести: true если "num простое число" или
+    // false если "num непростое число"
 }
 
 // Сделать предыдущие функции с пользовательским вводом используя prompt и alert
@@ -56,6 +56,3 @@ function isPrime(num) {
 // Написать функцию, которая выведет числа Фибоначчи до 100
 
 // Написать функцию, которая складывает числа по модулю
-
-// Добавить проверку валидности данных для предыдущих функций
-
