73c51c75cbee4de994c4c1b60b56904b4e0ac66d
loginovw8 <loginovw8@gmail.com>
2023-04-25 15:05:58 +0300

    Lesson 13 modified.

--- 13-cookies-sessions/app.js
+++ 13-cookies-sessions/app.js
@@ -1,108 +1,110 @@
 const express = require('express')
 const mysql = require('mysql');
-const path = require('path')
+const path = require('path');
 const session = require('express-session');
-const app = express()
+const app = express();
 
 // Соединение с базой данных
 const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  database: "nature",
-  user: "root",
-  password: "secret"
+    host: "127.0.0.1",
+    database: "nature",
+    user: "root",
+    password: "secret",
 });
 
-connection.connect(function (err) { if (err) throw err; });
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+});
 
 // Путь к директории файлов ресурсов (css, js, images)
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Настройка шаблонизатора
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // Путь к директории файлов отображения контента
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
 // Обработка POST-запросов из форм
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // Инициализация сессии
-app.use(session({secret: "Secret", resave: false, saveUninitialized: true}));
+app.use(session({ secret: "Secret", resave: false, saveUninitialized: true }));
 
 // Middleware
 function isAuth(req, res, next) {
-  if (req.session.auth) {
-    next();
-  } else {
-    res.redirect('/');
-  }
-}
+    if (req.session.auth) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+};
 
 // Запуск веб-сервера по адресу http://localhost:3000
-app.listen(3000)
+app.listen(3000);
 
 /**
  * Маршруты
  */
 app.get('/', (req, res) => {
-  connection.query("SELECT * FROM items", (err, data, fields) => {
-    if (err) throw err;
+    connection.query("SELECT * FROM items", (err, data, fields) => {
+        if (err) {
+            console.log(err);
+        }
 
-    res.render('home', {
-      items: data,
-      auth: req.session.auth
+        res.render('home', {
+            items: data,
+            auth: req.session.auth,
+        });
     });
-  });
-})
+});
 
-app.get('/items/:id', (req, res) => {
-  connection.query("SELECT * FROM items WHERE id=?", [req.params.id],
-    (err, data, fields) => {
-      if (err) throw err;
+app.get('/add', isAuth, (req, res) => {
+    res.render('add', {
+        auth: req.session.auth,
+    });
+});
 
-      res.render('item', {
-        item: data[0],
-        auth: req.session.auth
-      })
-  });
-})
+app.post('/store', isAuth, (req, res) => {
+    connection.query(
+        "INSERT INTO items (title, image) VALUES (?, ?)",
+        [[req.body.title], [req.body.image]],
+        (err, data, fields) => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.redirect('/');
+        }
+    );
+});
 
 app.get('/auth', (req, res) => {
-  res.render('auth', {
-    auth: req.session.auth
-  });
+    res.render('auth', {
+        auth: req.session.auth,
+    });
 });
-
-app.get('/add', (req, res) => {
-  res.render('add', {
-    auth: req.session.auth
-  })
-})
-
-app.get('/lock', isAuth, (req, res) => {
-  res.render('lock', {
-    auth: req.session.auth
-  });
-});
-
-app.post('/store', (req, res) => {
-  connection.query(
-    "INSERT INTO items (title, image) VALUES (?, ?)",
-    [[req.body.title], [req.body.image]], (err, data, fields) => {
-      if (err) throw err;
-
-      res.redirect('/')
-  });
-})
 
 app.post('/register', (req, res) => {
- connection.query(
-    "INSERT INTO users (name, password) VALUES (?, ?)",
-    [[req.body.name], [req.body.password]], (err, data, fields) => {
-      if (err) throw err;
+    connection.query(
+        "INSERT INTO users (name, password) VALUES (?, ?)",
+        [[req.body.name], [req.body.password]],
+        (err, data, fields) => {
+            if (err) {
+                console.log(err);
+            }
 
-      req.session.auth = true;
+            req.session.auth = true;
 
-      res.redirect('/')
-  });
+            res.redirect('/');
+        }
+    );
+});
+
+app.post('/logout', isAuth, (req, res) => {
+    req.session.auth = false;
+
+    res.redirect('/');
 });
